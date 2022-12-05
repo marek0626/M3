28b7fad23d0bdf61eaae6bfb801a3b1551db5b0a
Oliver Portee <oliver.portee@gmail.com>
2022-12-05 13:01:24 +0100

    lxclient wip (cleanup code, refined noop benchmark, more ioctl calls)

--- src/m3lx/src/lxclient/src/main.rs
+++ src/m3lx/src/lxclient/src/main.rs
@@ -1,19 +1,19 @@
-mod mmap;
-
 use base::{
     cfg,
     errors::Error,
     kif,
     tcu::{self, ActId, EpId},
+    time::Runner,
 };
-use mmap::Mmap;
 use std::os::unix::prelude::AsRawFd;
+use util::mmap::Mmap;
 
 // this is defined in linux/drivers/tcu.cc (and the right value will be printed on driver initialization during boot time)
 const IOCTL_RGSTR_ACT: u64 = 0x40087101;
-// const IOCTL_TO_TMX_MD: u64 = 0x7102;
-const IOCTL_TO_USR_MD: u64 = 0x7103;
+const IOCTL_TO_TMX_MD: u64 = 0x00007102;
+const IOCTL_TO_USR_MD: u64 = 0x00007103;
 const IOCTL_TLB_INSRT: u64 = 0x40087104;
+const IOCTL_UNREG_ACT: u64 = 0x00007105;
 
 const MSG_BUF_ADDR: usize = 0x4000_0000;
 const TM_RCV_BUF_ADDR: usize = cfg::TILEMUX_RBUF_SPACE;
@@ -67,6 +67,29 @@ fn switch_to_user_mode() {
     }
 }
 
+fn switch_to_tm_mode() {
+    let tcu_dev = std::fs::File::open("/dev/tcu").unwrap();
+    unsafe {
+        let res = libc::ioctl(tcu_dev.as_raw_fd(), IOCTL_TO_TMX_MD);
+        if res != 0 {
+            libc::perror(0 as *const u8);
+            panic!("ioctl for switching to tm mode failed");
+        }
+    }
+}
+
+fn exit() {
+    let tcu_dev = std::fs::File::open("/dev/tcu").unwrap();
+    unsafe {
+        let res = libc::ioctl(tcu_dev.as_raw_fd(), IOCTL_UNREG_ACT);
+        if res != 0 {
+            libc::perror(0 as *const u8);
+            panic!("ioctl for unregistering activity failed");
+        }
+    }
+}
+
+#[inline(always)]
 fn send_msg<T>(msg_obj: T, sep: EpId, rep: EpId) -> Result<(), Error> {
     let size = std::mem::size_of_val(&msg_obj);
     // let algn = std::mem::align_of_val(&msg_obj);
@@ -76,6 +99,7 @@ fn send_msg<T>(msg_obj: T, sep: EpId, rep: EpId) -> Result<(), Error> {
     tcu::TCU::send_aligned(sep, MSG_BUF_ADDR as *const u8, size, 0, rep)
 }
 
+#[inline(always)]
 fn wait_for_rpl<T>(rep: EpId, rcv_buf: usize) -> Result<&'static T, Error> {
     loop {
         if let Some(off) = tcu::TCU::fetch_msg(rep) {
@@ -100,27 +124,40 @@ fn send_receive_lx_act() -> ActId {
     rpl.actid as ActId
 }
 
-fn main() -> Result<(), std::io::Error> {
-    // these need to stay in scope so that the mmaped areas stay alive
-    let _tcu_mmap = Mmap::new("/dev/tcu", tcu::MMIO_ADDR, tcu::MMIO_ADDR, tcu::MMIO_SIZE)?;
-    let _msg_mmap = Mmap::new("/dev/mem", MSG_BUF_ADDR, MSG_BUF_ADDR, cfg::PAGE_SIZE)?;
-    let _tm_rcv_mmap = Mmap::new("/dev/mem", TM_RCV_BUF_ADDR, TM_RCV_BUF_ADDR, cfg::PAGE_SIZE)?;
-    let _us_rcv_mmap = Mmap::new("/dev/mem", US_RCV_BUF_ADDR, US_RCV_BUF_ADDR, cfg::PAGE_SIZE)?;
+// send and receive Exit sidecall
+fn send_receive_exit(id: ActId) {
+    let msg = kif::tilemux::Exit {
+        op: kif::tilemux::Calls::EXIT.val,
+        act_sel: id as u64,
+        code: 0,
+    };
+    send_msg(msg, tcu::KPEX_SEP, tcu::KPEX_REP).unwrap();
+    wait_for_rpl::<kif::DefaultReply>(tcu::KPEX_REP, TM_RCV_BUF_ADDR).unwrap();
+}
 
-    // insert tlb for tm for msg buf
-    tlb_insert_addr(MSG_BUF_ADDR, MSG_BUF_ADDR);
-    // send ActLx sidecall
-    let actid = send_receive_lx_act();
-    println!("actid: {}", actid);
-    // register act in linux kernel
-    register_act(actid);
-    // switch to user mode
-    switch_to_user_mode();
-    // insert tlb for user for msg buf
-    tlb_insert_addr(MSG_BUF_ADDR, MSG_BUF_ADDR);
+struct Tester;
 
-    println!("setup done");
+impl Runner for Tester {
+    fn pre(&mut self) {
+        let noop = kif::syscalls::Noop {
+            opcode: kif::syscalls::Operation::NOOP.val,
+        };
+        send_msg(
+            noop,
+            tcu::FIRST_USER_EP + tcu::SYSC_SEP_OFF,
+            tcu::FIRST_USER_EP + tcu::SYSC_REP_OFF,
+        )
+        .unwrap();
+    }
 
+    fn run(&mut self) {
+        wait_for_rpl::<kif::DefaultReply>(tcu::FIRST_USER_EP + tcu::SYSC_REP_OFF, US_RCV_BUF_ADDR)
+            .unwrap();
+    }
+}
+
+#[inline(never)]
+fn noop_syscall() {
     let noop = kif::syscalls::Noop {
         opcode: kif::syscalls::Operation::NOOP.val,
     };
@@ -130,8 +167,42 @@ fn main() -> Result<(), std::io::Error> {
         tcu::FIRST_USER_EP + tcu::SYSC_REP_OFF,
     )
     .unwrap();
-    // TODO: now, we cast to DefaultReply twice :/
-    wait_for_rpl::<kif::DefaultReply>(tcu::FIRST_USER_EP + tcu::SYSC_REP_OFF, US_RCV_BUF_ADDR).unwrap();
+    wait_for_rpl::<kif::DefaultReply>(tcu::FIRST_USER_EP + tcu::SYSC_REP_OFF, US_RCV_BUF_ADDR)
+        .unwrap();
+}
+
+fn main() -> Result<(), std::io::Error> {
+    // these need to stay in scope so that the mmaped areas stay alive
+    let _tcu_mmap = Mmap::new("/dev/tcu", tcu::MMIO_ADDR, tcu::MMIO_ADDR, tcu::MMIO_SIZE)?;
+    let _msg_mmap = Mmap::new("/dev/mem", MSG_BUF_ADDR, MSG_BUF_ADDR, cfg::PAGE_SIZE)?;
+    let _tm_rcv_mmap = Mmap::new("/dev/mem", TM_RCV_BUF_ADDR, TM_RCV_BUF_ADDR, cfg::PAGE_SIZE)?;
+    let _us_rcv_mmap = Mmap::new("/dev/mem", US_RCV_BUF_ADDR, US_RCV_BUF_ADDR, cfg::PAGE_SIZE)?;
+
+    tlb_insert_addr(MSG_BUF_ADDR, MSG_BUF_ADDR);
+    let actid = send_receive_lx_act();
+    println!("actid: {}", actid);
+    register_act(actid);
+    switch_to_user_mode();
+    tlb_insert_addr(MSG_BUF_ADDR, MSG_BUF_ADDR);
+
+    println!("setup done.");
+
+    use base::time::{CycleInstant, Profiler};
+
+    let profiler = Profiler::default().warmup(50).repeats(1000);
+    let mut res = profiler.run::<CycleInstant, _>(|| {
+        noop_syscall();
+    });
+    // let mut res = profiler.runner::<CycleInstant, _>(&mut Tester);
+    println!("{}", res);
+    res.filter_outliers();
+    println!("{}", res);
+    noop_syscall();
+
+    // cleanup
+    switch_to_tm_mode();
+    send_receive_exit(actid);
+    exit();
 
     Ok(())
 }
