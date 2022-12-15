376c6e7d9ab754b4d8f54e8842097b21fe0bf9a9
Oliver Portee <oliver.portee@gmail.com>
2022-12-15 10:37:00 +0100

    simplebench: more benchmarks for writing to memory, empty function

--- src/m3lx/src/simplebench/src/simplebench.rs
+++ src/m3lx/src/simplebench/src/simplebench.rs
@@ -3,15 +3,30 @@ use base::tcu;
 use base::time::{CycleInstant, Profiler};
 use util::mmap::Mmap;
 
-
+#[allow(unused)]
 fn bench<F: FnMut()>(p: &Profiler, name: &str, f: F) {
     let mut res = p.run::<CycleInstant, _>(f);
     res.filter_outliers();
     println!("{}: {}", name, res);
 }
 
-fn main() -> Result<(), std::io::Error> {
-    let _tcu_mmap = Mmap::new("/dev/tcu", tcu::MMIO_ADDR, tcu::MMIO_ADDR, tcu::MMIO_SIZE)?;
+use std::arch::asm;
+
+#[allow(unused)]
+fn write_to_stack() {
+    let mut a: u64 = 3;
+    let val1: u64 = 0xbadc0ffee0ddf00c;
+    let val2: u64 = 0xbadc0ffee0ddf00d;
+    let addr: usize = &mut a as *mut u64 as usize;
+    unsafe {
+        asm!("sd {0}, ({1})", in(reg) val1, in(reg) addr, options(nostack));
+        asm!("sd {0}, ({1})", in(reg) val2, in(reg) addr, options(nostack));
+    }
+}
+
+#[allow(unused)]
+fn bench_writes() {
+    let _tcu_mmap = Mmap::new("/dev/tcu", tcu::MMIO_ADDR, tcu::MMIO_ADDR, tcu::MMIO_SIZE).unwrap();
 
     let p = Profiler::default().warmup(50).repeats(1000);
 
@@ -42,6 +57,49 @@ fn main() -> Result<(), std::io::Error> {
     bench(&p, "stack dereference", || unsafe {
         *(&mut a as *mut u64) = 5u64;
     });
+}
 
-    Ok(())
+#[allow(unused)]
+#[inline(always)]
+fn bench_sd_instruction() {
+    let mut a: u64 = 3;
+    let val: u64 = 0xbadc0ffee0ddf00d;
+    let addr: usize = &mut a as *mut u64 as usize;
+    for _ in 0..100 {
+        unsafe {
+            asm!("sd {0}, ({1})", in(reg) val, in(reg) addr, options(nostack));
+        }
+    }
+}
+
+#[allow(unused)]
+#[inline(always)]
+fn bench_dereference() {
+    let mut a: u64 = 3;
+    let val: u64 = 0xbadc0ffee0ddf00d;
+    for _ in 0..100 {
+        // TODO: this is optimized away
+        unsafe {
+            *(&mut a as *mut u64) = val;
+        }
+    }
+    unsafe {
+        *(&mut a as *mut u64) = 0xbadc0ffee0ddf00c;
+    }
+    let p = Profiler::default().warmup(50).repeats(1000);
+    bench(&p, "stack dereference", || unsafe {
+        *(&mut a as *mut u64) = val;
+    })
+}
+
+#[allow(unused)]
+#[inline(always)]
+fn bench_nothing() {
+    let p = Profiler::default().warmup(50).repeats(1000);
+    bench(&p, "empty function ", || {});
+}
+
+fn main() {
+    bench_nothing();
+    bench_writes();
 }
