9199936b0c1be411410bf33c9b8b9ce676c213e3
Oliver Portee <oliver.portee@gmail.com>
2023-01-12 21:34:07 +0100

    lxclient: m3fs read/write/meta benchmarks

--- src/m3lx/src/lxclient/src/lxclient.rs
+++ src/m3lx/src/lxclient/src/lxclient.rs
@@ -1,4 +1,5 @@
 use base::io::Write;
+use base::time::{TimeInstant, Runner, Instant};
 use base::{
     cfg,
     errors::Error,
@@ -9,7 +10,8 @@ use base::{
     tcu::{self, EpId},
     time::{CycleInstant, Profiler},
 };
-use m3::vfs::{OpenFlags, VFS};
+use m3::tiles::Activity;
+use m3::vfs::{OpenFlags, VFS, FileMode, GenericFile, FileRef};
 use util::mmap::Mmap;
 
 fn wait_for_rpl<T>(rep: EpId, rcv_buf: usize) -> Result<&'static T, Error> {
@@ -41,8 +43,8 @@ fn noop_syscall(rbuf: usize) {
     wait_for_rpl::<()>(tcu::FIRST_USER_EP + tcu::SYSC_REP_OFF, rbuf).unwrap();
 }
 
-fn bench<F: FnMut()>(profiler: &Profiler, name: &str, f: F) {
-    let mut res = profiler.run::<CycleInstant, _>(f);
+fn bench<T: Instant, F: FnMut()>(profiler: &Profiler, name: &str, f: F) {
+    let mut res = profiler.run::<T, _>(f);
     println!("\n\n{}: {:?}", name, res);
     println!("{}: {}", name, res);
     res.filter_outliers();
@@ -50,40 +52,86 @@ fn bench<F: FnMut()>(profiler: &Profiler, name: &str, f: F) {
 }
 
 fn bench_custom_noop_syscall(profiler: &Profiler) {
-    let (rbuf, _) = m3::envdata::get().tile_desc().rbuf_std_space();
-    bench(profiler, "custom noop", || {
+    let (rbuf, _) = Activity::own().tile_desc().rbuf_std_space();
+    bench::<CycleInstant, _>(profiler, "custom noop", || {
         noop_syscall(rbuf);
     })
 }
 
 fn bench_m3_noop_syscall(profiler: &Profiler) {
-    bench(profiler, "m3 noop", || {
+    bench::<CycleInstant, _>(profiler, "m3 noop", || {
         m3::syscalls::noop().unwrap();
     })
 }
 
 fn bench_tlb_insert(profiler: &Profiler) {
     let sample_addr = profiler as *const Profiler as usize;
-    bench(profiler, "tlb insert", || {
+    bench::<CycleInstant, _>(profiler, "tlb insert", || {
         tcu::TCU::handle_xlate_fault(sample_addr, Perm::R);
     })
 }
 
-fn bench_m3fs(profiler: &Profiler) {
-    let new_file_contents = "test\ntest";
-    bench(profiler, "m3fs meta", || {
-        {
-            let mut file = VFS::open("/new-file.txt", OpenFlags::W | OpenFlags::CREATE).unwrap();
-            write!(file, "{}", new_file_contents).unwrap();
+const STR_LEN: usize = 512 * 1024;
+
+fn bench_m3fs_read(profiler: &Profiler) {
+    let mut file = VFS::open("/new-file.txt", OpenFlags::CREATE | OpenFlags::RW).unwrap();
+    let content: String = (0..STR_LEN).map(|_| "a").collect();
+    write!(file, "{}", content).unwrap();
+
+    bench::<TimeInstant, _>(profiler, "m3fs read", || {
+        let _content = file.read_to_string().unwrap();
+    });
+
+    VFS::unlink("/new-file.txt").unwrap();
+}
+
+struct WriteBenchmark {
+    file: FileRef<GenericFile>,
+    content: String,
+}
+
+impl WriteBenchmark {
+    fn new() -> WriteBenchmark {
+        WriteBenchmark {
+            file: VFS::open("/bla", OpenFlags::CREATE).unwrap(),
+            content: (0..STR_LEN).map(|_| "a").collect(),
         }
-        {
-            let mut file = VFS::open("/new-file.txt", OpenFlags::R).unwrap();
-            let contents = file.read_to_string().unwrap();
-            assert!(contents == new_file_contents);
-        }
-        {
-            VFS::unlink("/new-file.txt").unwrap();
-        }
+    }
+}
+
+impl Runner for WriteBenchmark {
+    fn pre(&mut self) {
+        self.file = VFS::open("/new-file.txt", OpenFlags::CREATE | OpenFlags::W).unwrap();
+    }
+
+    fn run(&mut self) {
+        write!(self.file, "{}", self.content).unwrap();
+    }
+
+    fn post(&mut self) {
+        VFS::unlink("/new-file.txt").unwrap();
+    }
+}
+
+fn bench_m3fs_write(profiler: &Profiler) {
+    let mut res = profiler.runner::<TimeInstant, _>(&mut WriteBenchmark::new());
+    let name = "m3fs write";
+    println!("\n\n{}: {:?}", name, res);
+    println!("{}: {}", name, res);
+    res.filter_outliers();
+    println!("{} filtered: {}", name, res);
+}
+
+fn bench_m3fs_meta(profiler: &Profiler) {
+    bench::<TimeInstant, _>(profiler, "m3fs meta", || {
+        VFS::mkdir("/new-dir", FileMode::from_bits(0o755).unwrap()).unwrap();
+        let _ = VFS::open("/new-dir/new-file", OpenFlags::CREATE).unwrap();
+        VFS::link("/new-dir/new-file", "/new-link").unwrap();
+        VFS::rename("/new-link", "/new-blink").unwrap();
+        let _ = VFS::stat("/new-blink").unwrap();
+        VFS::unlink("/new-blink").unwrap();
+        VFS::unlink("/new-dir/new-file").unwrap();
+        VFS::rmdir("/new-dir").unwrap();
     })
 }
 
@@ -96,7 +144,6 @@ fn main() -> Result<(), std::io::Error> {
     let env_page_off = cfg::ENV_START & !cfg::PAGE_MASK;
     let _env_mmap = Mmap::new("/dev/mem", env_page_off, env_page_off, cfg::ENV_SIZE)?;
     let env = m3::envdata::get();
-    println!("{:#?}", env);
 
     let rbuf_phys_addr = cfg::MEM_OFFSET + 2 * cfg::PAGE_SIZE;
     let (rbuf_virt_addr, rbuf_size) = env.tile_desc().rbuf_std_space();
@@ -108,11 +155,13 @@ fn main() -> Result<(), std::io::Error> {
     println!("setup done");
     VFS::mount("/", "m3fs", "m3fs").unwrap();
 
-    let profiler = Profiler::default().warmup(50).repeats(1000);
+    let profiler = Profiler::default().warmup(50).repeats(500);
     bench_custom_noop_syscall(&profiler);
     bench_m3_noop_syscall(&profiler);
     bench_tlb_insert(&profiler);
-    bench_m3fs(&profiler);
+    bench_m3fs_read(&profiler);
+    bench_m3fs_write(&profiler);
+    bench_m3fs_meta(&profiler);
 
     // cleanup
     ioctl::unregister_act();
