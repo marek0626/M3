1db8071adc70aaf0042e829915270b01f0cbf8b3
Nils Asmussen <nils@os.inf.tu-dresden.de>
2023-08-31 14:58:38 +0200

    Added basics to support multiple activities on Linux.

--- src/m3lx/apps/starter/src/starter.rs
+++ src/m3lx/apps/starter/src/starter.rs
@@ -16,47 +16,54 @@
 use base::env;
 use base::linux::{self, ioctl};
 
+use std::thread;
+
 fn main() -> Result<(), std::io::Error> {
-    linux::init();
+    linux::init_fd();
 
-    ioctl::register_act();
+    loop {
+        let id = ioctl::wait_act();
 
-    if unsafe { libc::fork() } == 0 {
-        let name = env::args().next().unwrap();
-        let mut bytes = [0u8; 1024];
-        let mut argv: Vec<*const u8> = vec![];
-        let mut pos = 0;
-        for arg in env::args() {
-            // store null-terminated argument into bytes array
-            bytes[pos..pos + arg.len()].copy_from_slice(arg.as_bytes());
-            bytes[pos + arg.len()] = b'\0';
-            // store pointer in argv array
-            unsafe {
-                argv.push(bytes.as_ptr().add(pos));
+        let pid = unsafe { libc::fork() };
+        if pid == 0 {
+            ioctl::register_act(id);
+            linux::init_env();
+
+            let name = env::args().next().unwrap();
+            let mut bytes = [0u8; 1024];
+            let mut argv: Vec<*const u8> = vec![];
+            let mut pos = 0;
+            for arg in env::args() {
+                // store null-terminated argument into bytes array
+                bytes[pos..pos + arg.len()].copy_from_slice(arg.as_bytes());
+                bytes[pos + arg.len()] = b'\0';
+                // store pointer in argv array
+                unsafe {
+                    argv.push(bytes.as_ptr().add(pos));
+                }
+                pos += arg.len() + 1;
             }
-            pos += arg.len() + 1;
+            argv.push(std::ptr::null());
+
+            println!(
+                "Running {} {:?} in process {}",
+                name,
+                env::args().skip(1).collect::<Vec<_>>(),
+                unsafe { libc::getpid() },
+            );
+
+            let res = unsafe { libc::execvp(name.as_ptr(), argv.as_ptr()) };
+            if res != 0 {
+                println!("execvp failed: {}", res);
+            }
         }
-        argv.push(std::ptr::null());
-
-        println!(
-            "Running {} {:?} in process {}",
-            name,
-            env::args().skip(1).collect::<Vec<_>>(),
-            unsafe { libc::getpid() },
-        );
-
-        let res = unsafe { libc::execvp(name.as_ptr(), argv.as_ptr()) };
-        if res != 0 {
-            println!("execvp failed: {}", res);
+        else {
+            thread::spawn(move || {
+                let mut status = 0;
+                unsafe { libc::waitpid(pid, &mut status as *mut _, 0) };
+                println!("Process {} exited with status {}", pid, status);
+                ioctl::unregister_act(id);
+            });
         }
     }
-    else {
-        let mut status = 0;
-        let pid = unsafe { libc::wait(&mut status as *mut _) };
-        println!("Process {} exited with status {}", pid, status);
-    }
-
-    ioctl::unregister_act();
-
-    Ok(())
 }
