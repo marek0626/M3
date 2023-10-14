b6fb6b46335aaebe657e751aeba3ba6fcc064d2e
Stephan Gerhold <stephan.gerhold@mailbox.tu-dresden.de>
2023-10-14 20:28:08 +0200

    Add CFIDev tile ISA for tiles with CFI flash
    Add a dummy ISA for tiles with CFI flash controller as very simple form
    of non-volatile storage. This is mainly intended for gem5 where a CFI
    flash emulation is available. In the hardware prototype, a different
    kind of flash interface might be easier to integrate.
    
    The flash can be easily read just like regular memory. For writing,
    a driver would need to write special commands into the MMIO region.
    This is not supported yet.

--- src/libs/rust/base/src/kif/tiledesc.rs
+++ src/libs/rust/base/src/kif/tiledesc.rs
@@ -61,6 +61,8 @@ pub enum TileISA {
     NICDev,
     /// Dummy ISA to represent the serial input device
     SerialDev,
+    /// Dummy ISA to represent a flash with the Common Flash Interface (CFI)
+    CFIDev,
 }
 
 bitflags! {
@@ -182,6 +184,7 @@ impl TileDesc {
         self.isa() == TileISA::NICDev
             || self.isa() == TileISA::IDEDev
             || self.isa() == TileISA::SerialDev
+            || self.isa() == TileISA::CFIDev
     }
 
     /// Return if the tile supports activities
@@ -304,6 +307,14 @@ impl TileDesc {
                     res = TileDesc::new_with_attr(
                         TileType::Comp,
                         TileISA::SerialDev,
+                        0,
+                        TileAttr::empty(),
+                    )
+                },
+                "cfidev" => {
+                    res = TileDesc::new_with_attr(
+                        TileType::Comp,
+                        TileISA::CFIDev,
                         0,
                         TileAttr::empty(),
                     )
