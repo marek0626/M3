4709491b6f439cf8ebef4f67f5b48a402469687f
Nils Asmussen <nils@os.inf.tu-dresden.de>
2024-05-27 13:27:54 +0200

    isr: added missing feature flags.

--- src/libs/rust/isr/build.py
+++ src/libs/rust/isr/build.py
@@ -9,4 +9,4 @@ def build(gen, env):
             lib = env.static_lib(gen, out='isr-' + isa + '-' + str(sf), ins=files)
             env.install_as(gen, env['LIBDIR'] + '/libisr.a', lib)
 
-    env.m3_rust_lib(gen)
+    env.m3_rust_lib(gen, features=["isr/" + env['TGT']])
