9cb5571b0e36456db16f916121006aeaa404f1d8
Nils Asmussen <nils@os.inf.tu-dresden.de>
2024-08-23 16:24:58 +0200

    librustbase: added tests for the variable ring buffer.

--- src/libs/rust/base/src/col/ringbuf.rs
+++ src/libs/rust/base/src/col/ringbuf.rs
@@ -131,5 +131,127 @@ impl VarRingBuf {
         self.size -= size;
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basics() {
+        let mut rb = VarRingBuf::new(10);
+        assert_eq!(rb.capacity(), 10);
+        assert!(rb.empty());
+        rb.get_write_pos(1).unwrap();
+        rb.push(1, 1);
+        assert!(!rb.empty());
+        assert_eq!(rb.size(), 1);
+        assert_eq!(rb.get_write_pos(10), None);
+
+        let (_rpos, rlen) = rb.get_read_pos(4).unwrap();
+        assert_eq!(rlen, 1);
+        rb.pull(rlen);
+
+        assert!(rb.empty());
+        rb.get_write_pos(10);
+    }
+
+    #[test]
+    fn limits() {
+        let mut rb = VarRingBuf::new(10);
+        assert_eq!(rb.capacity(), 10);
+        assert!(rb.empty());
+        assert_eq!(rb.get_read_pos(1), None);
+
+        let wpos = rb.get_write_pos(10).unwrap();
+        assert_eq!(wpos, 0);
+        rb.push(10, 10);
+        assert!(!rb.empty());
+        assert_eq!(rb.size(), 10);
+
+        let (rpos, rlen) = rb.get_read_pos(10).unwrap();
+        assert_eq!(rpos, 0);
+        assert_eq!(rlen, 10);
+        rb.pull(rlen);
+
+        assert!(rb.empty());
+    }
+
+    #[test]
+    fn small_steps() {
+        let mut rb = VarRingBuf::new(4);
+
+        rb.get_write_pos(1).unwrap();
+        rb.push(1, 1);
+        rb.get_write_pos(1).unwrap();
+        rb.push(1, 1);
+        rb.get_write_pos(1).unwrap();
+        rb.push(1, 1);
+        rb.get_write_pos(1).unwrap();
+        rb.push(1, 1);
+        assert_eq!(rb.get_write_pos(1), None);
+
+        rb.get_read_pos(1).unwrap();
+        rb.pull(1);
+        rb.get_read_pos(1).unwrap();
+        rb.pull(1);
+        rb.get_read_pos(1).unwrap();
+        rb.pull(1);
+        rb.get_read_pos(1).unwrap();
+        rb.pull(1);
+        assert!(rb.empty());
+    }
+
+    #[test]
+    fn wrap_around() {
+        let mut rb = VarRingBuf::new(4);
+        assert_eq!(rb.get_write_pos(2).unwrap(), 0);
+        rb.push(2, 2);
+        assert_eq!(rb.get_read_pos(2).unwrap(), (0, 2));
+        rb.pull(2);
+        assert_eq!(rb.get_write_pos(2).unwrap(), 2);
+        rb.push(2, 2);
+        assert_eq!(rb.get_read_pos(2).unwrap(), (2, 2));
+        rb.pull(2);
+        assert_eq!(rb.get_write_pos(2).unwrap(), 0);
+        rb.push(2, 2);
+        assert_eq!(rb.get_read_pos(2).unwrap(), (0, 2));
+        rb.pull(2);
+        assert!(rb.empty());
+    }
+
+    #[test]
+    fn diff_steps() {
+        let mut rb = VarRingBuf::new(4);
+        assert_eq!(rb.get_write_pos(4).unwrap(), 0);
+        rb.push(4, 4);
+        assert_eq!(rb.get_read_pos(2).unwrap(), (0, 2));
+        rb.pull(2);
+        assert_eq!(rb.get_read_pos(2).unwrap(), (2, 2));
+        rb.pull(2);
+        assert!(rb.empty());
+        assert_eq!(rb.get_write_pos(4).unwrap(), 0);
+        rb.push(4, 4);
+        assert_eq!(rb.get_read_pos(3).unwrap(), (0, 3));
+        rb.pull(3);
+        assert_eq!(rb.get_write_pos(1).unwrap(), 0);
+        rb.push(1, 1);
+        assert_eq!(rb.get_read_pos(4).unwrap(), (3, 1));
+        rb.pull(1);
+        assert_eq!(rb.get_read_pos(2).unwrap(), (0, 1));
+        rb.pull(1);
+        assert!(rb.empty());
+    }
+
+    #[test]
+    fn read_behind() {
+        let mut rb = VarRingBuf::new(6);
+        assert_eq!(rb.get_write_pos(6).unwrap(), 0);
+        rb.push(6, 6);
+        assert_eq!(rb.get_read_pos(4).unwrap(), (0, 4));
+        rb.pull(4);
+        assert_eq!(rb.get_write_pos(2).unwrap(), 0);
+        rb.push(2, 2);
+        assert_eq!(rb.get_write_pos(2).unwrap(), 2);
+        rb.push(2, 2);
     }
 }
