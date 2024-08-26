ba1864ecdabae8cff36d1ce7947e6998c7aaa2d7
Nils Asmussen <nils@os.inf.tu-dresden.de>
2024-08-26 11:36:48 +0200

    librustbase: expect pointer for BoxList::move_to_back.
    As we need a mutable reference to the list, we should not expect a
    mutable reference to an element of the list as this cannot be obtained
    safely. All current usages keep pointers to the elements separately,
    though, and therefore sidestep this problem.

--- src/libs/rust/base/src/col/boxlist.rs
+++ src/libs/rust/base/src/col/boxlist.rs
@@ -313,27 +313,27 @@ impl<T: BoxItem> BoxList<T> {
     /// # Safety
     ///
     /// This function assumes that the given element is part of this list
-    pub unsafe fn move_to_back(&mut self, item: &mut T) {
+    pub unsafe fn move_to_back(&mut self, item: *mut T) {
         // already at the back? (tail is always Some, because T is in the list)
-        if self.tail.unwrap().as_ptr() == item as *mut T {
+        if self.tail.unwrap().as_ptr() == item {
             return;
         }
 
         // remove us from the list
-        match item.prev() {
-            Some(mut p) => p.as_mut().set_next(item.next()),
-            None => self.head = item.next(),
+        match (*item).prev() {
+            Some(mut p) => p.as_mut().set_next((*item).next()),
+            None => self.head = (*item).next(),
         }
         // it's not at the back, so we can assume next() is Some
-        item.next().unwrap().as_mut().set_prev(item.prev());
+        (*item).next().unwrap().as_mut().set_prev((*item).prev());
 
         // let the current tail's next point to us
-        let item_ptr = Some(NonNull::new_unchecked(item as *mut T));
+        let item_ptr = Some(NonNull::new_unchecked(item));
         self.tail.unwrap().as_mut().set_next(item_ptr);
 
         // add us to the end
-        item.set_prev(self.tail);
-        item.set_next(None);
+        (*item).set_prev(self.tail);
+        (*item).set_next(None);
         self.tail = item_ptr;
     }
 }
