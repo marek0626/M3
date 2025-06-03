5af810796ee2fd07421af763dcd5a0d44591f33b
Nils Asmussen <nils@os.inf.tu-dresden.de>
2025-06-03 14:55:21 +0200

    rust: specify rerun-if-env-changed just to be sure.

--- src/build.rs
+++ src/build.rs
@@ -3,6 +3,8 @@ use std::env;
 fn main() {
     let build = env::var("M3_BUILD").unwrap();
     let target = env::var("M3_TARGET").unwrap();
+    println!("cargo::rerun-if-env-changed=M3_BUILD");
+    println!("cargo::rerun-if-env-changed=M3_TARGET");
     println!("cargo::rustc-env=M3_BUILD={}", build);
     println!("cargo::rustc-cfg=M3_BUILD=\"{}\"", build);
     println!("cargo::rustc-env=M3_TARGET={}", target);
