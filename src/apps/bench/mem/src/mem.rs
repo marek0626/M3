5627fa0e8f253e63f4227f2a9e8f60e7e6943b64
Nils Asmussen <nils@os.inf.tu-dresden.de>
2025-08-26 11:53:07 +0200

    bench-mem: fixed mode argument.

--- src/apps/bench/mem/src/mem.rs
+++ src/apps/bench/mem/src/mem.rs
@@ -51,7 +51,7 @@ impl FromStr for Mode {
 
 #[no_mangle]
 pub fn main() -> Result<(), Error> {
-    let mode: Mode = Mode::Random;
+    let mode: Mode = env::args().nth(1).unwrap().parse().unwrap();
     let size = env::args().nth(2).map(|s| s.parse::<usize>().unwrap());
 
     let buf = vec![0u8; 1024 * 1024];
