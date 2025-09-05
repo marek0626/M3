7d1edd46fdd11deeb2967903021f208972b90361
Nils Asmussen <nils@os.inf.tu-dresden.de>
2025-09-05 09:48:22 +0200

    buildroot: updated to 2025.05.1.

--- build.py
+++ build.py
@@ -48,7 +48,7 @@ class M3Env(Env):
         rustabi = 'musl'
         cross = isa + '-buildroot-linux-musl-'
         crossdir = os.path.abspath('build/cross-' + isa + '/host')
-        crossver = '14.2.0'
+        crossver = '14.3.0'
         env['CROSS'] = cross
         env['CROSSDIR'] = crossdir
         env['CROSSVER'] = crossver
