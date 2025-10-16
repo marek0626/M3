48ddd56ecf8e87c239a9230fb54c0b58b3934f48
Felix Guttbier <felix.guttbier@mailbox.tu-dresden.de>
2025-10-16 18:07:38 +0200

    librustbase: remap tcu on sigsegv

--- src/libs/rust/base/src/arch/linux/mod.rs
+++ src/libs/rust/base/src/arch/linux/mod.rs
@@ -2,6 +2,7 @@ pub mod ioctl;
 pub mod mmap;
 
 use std::fs::{File, OpenOptions};
+use std::mem::MaybeUninit;
 use std::os::unix::fs::OpenOptionsExt;
 use std::os::unix::io::AsRawFd;
 
@@ -70,19 +71,67 @@ pub fn init_env() {
     .expect("Unable to map environment");
 }
 
+extern "C" fn handle_sigsegv(
+    _sig: libc::c_int,
+    sig_info: *mut libc::siginfo_t,
+    _ucontext_void: *mut libc::c_void,
+) -> () {
+    if sig_info.is_null() {
+        unsafe {
+            let _ = libc::_exit(1);
+        }
+    }
+    let sig_info = unsafe { *sig_info };
+    let si_addr = unsafe { sig_info.si_addr() };
+    if si_addr.is_null() {
+        unsafe {
+            let _ = libc::_exit(1);
+        }
+    }
+    let si_addr = si_addr as usize as u64;
+    if si_addr >= tcu::MMIO_ADDR.as_raw() as u64
+        && si_addr < (tcu::MMIO_ADDR.as_raw() as u64 + tcu::MMIO_SIZE as u64)
+    {
+        mmap::mmap_tcu(
+            tcu_fd(),
+            tcu::MMIO_ADDR,
+            tcu::MMIO_SIZE,
+            mmap::MemType::TCU,
+            Perm::RW,
+        )
+        .expect("Unable to map TCU MMIO region");
+    }
+    else {
+        unsafe {
+            let _ = libc::_exit(1);
+        }
+    }
+}
+
+fn install_sigsegv_handler() {
+    unsafe {
+        let mut mask: MaybeUninit<libc::sigset_t> = MaybeUninit::uninit();
+        _ = libc::sigemptyset(mask.as_mut_ptr());
+
+        let new_action = libc::sigaction {
+            sa_sigaction: handle_sigsegv as *const fn() as *const libc::c_void as usize,
+            sa_mask: mask.assume_init(),
+            sa_flags: libc::SA_SIGINFO,
+            sa_restorer: None,
+        };
+
+        let mut old_action: MaybeUninit<libc::sigaction> = MaybeUninit::uninit();
+
+        libc::sigaction(libc::SIGSEGV, &new_action, old_action.as_mut_ptr());
+    }
+}
+
 pub fn init() {
     init_fd();
 
     init_env();
 
-    mmap::mmap_tcu(
-        tcu_fd(),
-        tcu::MMIO_ADDR,
-        tcu::MMIO_SIZE,
-        mmap::MemType::TCU,
-        Perm::RW,
-    )
-    .expect("Unable to map TCU MMIO region");
+    install_sigsegv_handler();
 
     #[cfg(not(M3_TARGET = "hw23"))]
     mmap::mmap_tcu(
