b25a6effd528e19b2bbf06bf34b98be397b8cc42
Nils Asmussen <nils@os.inf.tu-dresden.de>
2025-11-10 14:41:03 +0100

    libbase: support C++-based M³Linux apps again.

--- src/libs/base/arch/linux/Init.cc
+++ src/libs/base/arch/linux/Init.cc
@@ -21,6 +21,7 @@
 #include <base/arch/linux/MMap.h>
 
 #include <fcntl.h>
+#include <signal.h>
 #include <sys/epoll.h>
 #include <unistd.h>
 
@@ -41,10 +42,44 @@ int tcu_fd() {
     return lxdev.fd;
 }
 
+static void handle_sigsegv(int sig, siginfo_t *sig_info, void *ucontext) {
+    (void)sig;
+    (void)ucontext;
+
+    if(sig_info == nullptr)
+        _exit(1);
+
+    void *addr = sig_info->si_addr;
+    if(addr == nullptr)
+        _exit(1);
+
+    uintptr_t addr_int = reinterpret_cast<uintptr_t>(addr);
+    if(addr_int >= m3::TCU::MMIO_ADDR && addr_int < (m3::TCU::MMIO_ADDR + m3::TCU::MMIO_SIZE))
+        mmap_tcu(tcu_fd(), reinterpret_cast<void *>(m3::TCU::MMIO_ADDR), m3::TCU::MMIO_SIZE,
+                 MemType::TCU, m3::KIF::Perm::RW);
+    else
+        _exit(1);
+}
+
+void install_sig_handler() {
+    sigset_t mask;
+    sigemptyset(&mask);
+
+    struct sigaction new_action;
+    memset(&new_action, 0, sizeof(new_action));
+    new_action.sa_sigaction = handle_sigsegv;
+    new_action.sa_mask = mask;
+    new_action.sa_flags = SA_SIGINFO;
+
+    struct sigaction old_action;
+
+    sigaction(SIGSEGV, &new_action, &old_action);
+    sigaction(SIGBUS, &new_action, &old_action);
+}
+
 LinuxInit::LinuxInit() : fd(init_dev()) {
-    mmap_tcu(fd, reinterpret_cast<void *>(m3::TCU::MMIO_ADDR), m3::TCU::MMIO_SIZE, MemType::TCU,
-             m3::KIF::Perm::RW);
     init_env(fd);
+    install_sig_handler();
 #if defined(__hw__) || defined(__gem5__)
     mmap_tcu(fd, reinterpret_cast<void *>(m3::TCU::MMIO_EPS_ADDR), m3::TCU::endpoints_size(),
              MemType::TCUEps, m3::KIF::Perm::R);
