45a25094a6bc43921cc9c6c34703e91ae859943e
Nils Asmussen <nils@os.inf.tu-dresden.de>
2025-12-08 11:28:04 +0100

    lvldbserver: allow to use an shmem-based filesystem.

--- src/apps/bench/ycsb/lvldbserver/leveldb.cc
+++ src/apps/bench/ycsb/lvldbserver/leveldb.cc
@@ -17,6 +17,7 @@
 #include <base/time/Profile.h>
 
 #include <m3/Test.h>
+#include <m3/com/MemGate.h>
 #include <m3/session/Network.h>
 #include <m3/stream/Standard.h>
 #include <m3/vfs/VFS.h>
@@ -24,6 +25,7 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <unistd.h>
 
 #include "handler.h"
 #include "leveldb/db.h"
@@ -32,40 +34,71 @@
 
 using namespace m3;
 
+void usage(const char *prog) {
+    eprintln("Usage: {} [-s <shmem>] <db> <repeats> tcp <port>"_cf, prog);
+    eprintln("Usage: {} [-s <shmem>] <db> <repeats> tcu"_cf, prog);
+    eprintln("Usage: {} [-s <shmem>] <db> <repeats> udp <ip> <port> <workload>"_cf, prog);
+    exit(1);
+}
+
 int main(int argc, char **argv) {
-    if(argc != 4 && argc != 5 && argc != 7) {
-        eprintln("Usage: {} <db> <repeats> tcp <port>"_cf, argv[0]);
-        eprintln("Usage: {} <db> <repeats> tcu"_cf, argv[0]);
-        eprintln("Usage: {} <db> <repeats> udp <ip> <port> <workload>"_cf, argv[0]);
-        return 1;
+    const char *shmem_name = nullptr;
+
+    int opt;
+    while((opt = getopt(argc, argv, "s:")) != -1) {
+        switch(opt) {
+            case 's': shmem_name = optarg; break;
+            default: usage(argv[0]);
+        }
     }
+
+    int remaining = argc - optind;
+    if(remaining < 3)
+        usage(argv[0]);
 
     Network *net = nullptr;
 
+    // give ourself access to the shared memory area of the file system
+    MemCap *shmem = nullptr;
+    Reference<Tile> shmemtile;
+    if(shmem_name != nullptr) {
+        shmem = new MemCap(MemCap::attach_shmem(shmem_name));
+        shmemtile = Tile::from_shmem(shmem_name);
+        shmem->make_exclusive(shmemtile, Activity::own().tile(), false);
+    }
+
     VFS::mount("/", "m3fs", "m3fs");
 
-    const char *file = argv[1];
-    int repeats = IStringStream::read_from<int>(argv[2]);
+    // ensure that /tmp exists (necessary if the FS is empty)
+    FileInfo info;
+    if(VFS::try_stat("/tmp", info) != Errors::SUCCESS)
+        VFS::mkdir("/tmp", 0755);
 
-    Executor *exec = Executor::create(file);
+    const char *db = argv[optind + 0];
+    int repeats = IStringStream::read_from<int>(argv[optind + 1]);
+    std::string mode = argv[optind + 2];
 
-    println("Creating handler {}..."_cf, argv[3]);
+    Executor *exec = Executor::create(db);
+
+    println("Creating handler {}..."_cf, mode);
 
     OpHandler *hdl;
-    if(strcmp(argv[3], "tcp") == 0) {
-        port_t port = IStringStream::read_from<port_t>(argv[4]);
+    if(mode == "tcp") {
+        port_t port = IStringStream::read_from<port_t>(argv[optind + 3]);
         net = new Network("net");
         hdl = new TCPOpHandler(*net, port);
     }
-    else if(strcmp(argv[3], "udp") == 0) {
-        IpAddr ip = IStringStream::read_from<IpAddr>(argv[4]);
-        port_t port = IStringStream::read_from<port_t>(argv[5]);
-        const char *workload = argv[6];
+    else if(mode == "udp") {
+        IpAddr ip = IStringStream::read_from<IpAddr>(argv[optind + 3]);
+        port_t port = IStringStream::read_from<port_t>(argv[optind + 4]);
+        const char *workload = argv[optind + 5];
         net = new Network("net");
         hdl = new UDPOpHandler(*net, workload, ip, port);
     }
-    else
+    else if(mode == "tcu")
         hdl = new TCUOpHandler();
+    else
+        usage(argv[0]);
 
     println("Starting Benchmark:"_cf);
 
@@ -109,7 +142,7 @@ int main(int argc, char **argv) {
     }
 
     auto name = OStringStream();
-    format_to(name, "YCSB with {}"_cf, argv[3]);
+    format_to(name, "YCSB with {}"_cf, mode);
     WVPERF(name.str(), res);
 
     delete hdl;
