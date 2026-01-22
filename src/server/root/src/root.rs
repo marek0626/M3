32aafee16b2d09b45f77292d761e8e35aaf05acf
vilaureu <vilaureu@users.noreply.github.com>
2026-01-22 16:30:59 +0100

    root, pager: ignore additional async lint
    Passing workloop_async as a pointer triggers a different lint than calling it.
    This additional lint should also be ignored.

--- src/server/root/src/root.rs
+++ src/server/root/src/root.rs
@@ -274,7 +274,7 @@ fn workloop_async(args: &mut WorkloopArgs<'_, '_, '_, '_, '_>) {
 }
 
 #[no_mangle]
-#[cfg_attr(dylint_lib = "m3_lints", allow(unexpected_async))]
+#[cfg_attr(dylint_lib = "m3_lints", allow(async_alias, unexpected_async))]
 pub fn main() -> Result<(), Error> {
     let (sub, mut res) = subsys::Subsystem::new().expect("Unable to read subsystem info");
 
