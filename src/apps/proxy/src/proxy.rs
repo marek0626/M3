5bf83c88d8b1692ecb76d62dc68d0658a629a6d4
marek0626 <marek0626@gmail.com>
2026-05-25 13:48:45 +0200

    Successfully send a message to M3Linux

--- src/apps/proxy/src/proxy.rs
+++ src/apps/proxy/src/proxy.rs
@@ -1,9 +1,21 @@
 #![no_std]
-use m3::{errors::Error, println};
+use m3::{
+    com::{RecvGate, SendGate},
+    errors::Error,
+    println, send_recv,
+};
 #[unsafe(no_mangle)]
 pub fn main() -> Result<(), Error> {
     println!("=======================");
     println!("Proxy booting up");
     println!("=======================");
+    let sgate = SendGate::new_named("chan")?;
+    println!("Proxy connected to M3Linux rgate");
+
+    let val: u32 = 99;
+    println!("Proxy sending test value {}", val);
+
+    send_recv!(&sgate, RecvGate::def(), val)?;
+    println!("Proxy received acknowledgment from M3Linux");
     Ok(())
 }
