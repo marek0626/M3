05f690d59b01f3ec1b16b79c7e9f1535285fbc47
marek0626 <marek0626@gmail.com>
2026-06-09 12:16:36 +0200

    finished gates

--- src/m3lx/apps/proxy/src/proxy.rs
+++ src/m3lx/apps/proxy/src/proxy.rs
@@ -2,16 +2,17 @@
 extern crate m3core as m3;
 
 use m3::cap::{CapFlags, SelSpace};
-use m3::com::{RGateArgs, RecvCap, RecvGate, SGateArgs, SendCap};
+use m3::com::{RGateArgs, RecvCap, RecvGate, SGateArgs, SendCap, SendGate, opcodes};
 use m3::errors::Error;
 use m3::net::NetEventChannel;
 use m3::println;
 use m3::server::{ExcType, RequestHandler, RequestSession, Server, ServerSession};
-use m3::util::math;
+use m3::util::math::{self, next_log2};
 
 // Defining custom session state
 struct ProxySession {
     serv: ServerSession,
+    gates: Option<(RecvGate, SendCap)>,
 }
 
 // Implemenation for the request state which is called automatically
@@ -19,7 +20,7 @@ impl RequestSession for ProxySession {
     fn new(serv: ServerSession, arg: &str) -> Result<Self, Error> {
         println!(">>> PROXY: Received a new session connection! <<<");
         println!(">>> PROXY: Session args: {} <<<", arg);
-        Ok(ProxySession { serv })
+        Ok(ProxySession { serv, gates: None })
     }
 
     fn close(
@@ -49,7 +50,7 @@ pub fn main() -> Result<(), Error> {
     let mut srv = Server::new("proxy", &mut reqhdl)?;
 
     println!("Proxy successfully announced 'proxy' service and is waiting...");
-    reqhdl.reg_cap_handler(4, ExcType::Obt(2), |_clients, _crt, _sid, xchg| {
+    reqhdl.reg_cap_handler(opcodes::Net::Create as usize, ExcType::Obt(2), |_clients, _crt, _sid, xchg| {
         println!("Proxy caught create!");
 
         let sock_ty: u8 = xchg.in_args().pop()?;
@@ -69,26 +70,51 @@ pub fn main() -> Result<(), Error> {
 
         let sels = SelSpace::get().alloc_sels(2);
         let caps = sels.start();
-        let rgate_cli = RecvCap::new_with(
+
+        // The proxy gives the recvcap to the client at slot 0
+        let client_rgate = RecvCap::new_with(
             RGateArgs::default()
                 .sel(caps + 0)
                 .msg_order(math::next_log2(2048))
                 .order(math::next_log2(2048 * 4))
-                .flags(CapFlags::KEEP_CAP),
-        )?;
+                .flags(CapFlags::KEEP_CAP))?;
+        println!("Client rgate went through");
 
-        let _sgate_cli = SendCap::new_with(
-            SGateArgs::new(&rgate_cli)
-                .sel(caps + 1)
-                .credits(4)
-                .flags(CapFlags::KEEP_CAP),
-        )?;
+        // The proxy keeps a sgate pointing to it
+        let proxy_sgate = SendCap::new_with(SGateArgs::new(&client_rgate).credits(4))?;
+        println!("proxy sgate");
+
+        // The proxy creates and keeps an rgate
+        let proxy_rgate = RecvGate::new(math::next_log2(128), math::next_log2(32))?;
+        println!("proxy rgate");
+
+        // The proxy points the clients sgate to it
+        let client_sgate = SendCap::new_with(SGateArgs::new(&proxy_rgate)
+            .sel(caps + 1)
+            .credits(4)
+            .flags(CapFlags::KEEP_CAP))?;
+        println!("client sgate");
+
+        // send the ends to the client
         xchg.out_caps(sels);
+
+        println!("send cap to client");
+
+        // save the proxy's ends
+        let sess = _clients.get_mut(_sid).unwrap();
+        sess.gates = Some((proxy_rgate, proxy_sgate));
+        println!("saved gates");
 
         Ok(())
     });
 
-    reqhdl.reg_msg_handler(0, |_sess, msg| {
+    reqhdl.reg_msg_handler(opcodes::Net::GetIP as usize, |_sess, msg| {
+        let dummy_addr: u32 = 0;
+        m3::reply_vmsg!(msg, m3::errors::Code::Success, dummy_addr);
+        Ok(())
+    });
+
+    reqhdl.reg_msg_handler(opcodes::Net::Bind as usize, |_sess, msg| {
         let sd: usize = msg.pop()?;
         let port: u16 = msg.pop()?;
 
@@ -100,7 +126,7 @@ pub fn main() -> Result<(), Error> {
 
         Ok(())
     });
-    reqhdl.reg_msg_handler(6, |_sess, msg| {
+    reqhdl.reg_msg_handler(opcodes::Net::GetNameSrv as usize, |_sess, msg| {
         println!(">>> Proxy: GetNameServer requested");
 
         let dummy_addr: u32 = 0;
@@ -109,7 +135,7 @@ pub fn main() -> Result<(), Error> {
 
         Ok(())
     });
-    reqhdl.reg_msg_handler(1, |_sess, msg| {
+    reqhdl.reg_msg_handler(opcodes::Net::Listen as usize, |_sess, msg| {
         let sd: usize = msg.pop()?;
         let port: u16 = msg.pop()?;
 
@@ -121,7 +147,7 @@ pub fn main() -> Result<(), Error> {
 
         Ok(())
     });
-    reqhdl.reg_msg_handler(2, |_sess, msg| {
+    reqhdl.reg_msg_handler(opcodes::Net::Connect as usize, |_sess, msg| {
         let sd: usize = msg.pop()?;
         let addr: u32 = msg.pop()?;
         let port: u16 = msg.pop()?;
@@ -136,10 +162,11 @@ pub fn main() -> Result<(), Error> {
         m3::reply_vmsg!(msg, m3::errors::Code::Success, dummy_addr, dummy_port);
         Ok(())
     });
-    reqhdl.reg_msg_handler(3, |_sess, msg| {
+    reqhdl.reg_msg_handler(opcodes::Net::Abort as usize, |_sess, msg| {
         let sd: usize = msg.pop()?;
         let remove: bool = msg.pop()?;
 
+        // m3::log!();
         println!(
             ">>> Proxy: Abort requested on SD: {}, with Remove: {}",
             sd, remove
