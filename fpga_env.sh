00235f457288cf9caf78bd9f2fd69723ecdce51c
marek0626 <marek0626@gmail.com>
2026-06-11 16:32:17 +0200

    fpga

---
+++ fpga_env.sh
@@ -0,0 +1,11 @@
+# Dein SSH Alias
+export M3_HW_FPGA_HOST=studfpga
+
+# Verzeichnis im Home-Verzeichnis auf dem FPGA-PC
+export M3_HW_FPGA_DIR=m3
+
+# Die Nummer des FPGAs und damit seine IP-Adresse
+export M3_HW_FPGA_NO=2
+
+# Das Verzeichnis von Vivado-Lab
+export M3_HW_VIVADO="/opt/Xilinx/Vivado_Lab/2019.1/bin/vivado_lab"
